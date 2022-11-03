660d93ab7f342a5c1ca99e3915a631a96388d65f
danhillida <dan.hill.ida@gmail.com>
2022-11-03 19:40:15 -0400

    Started new functions section

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,57 @@
 'use strict';
+/*
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  //   numPassengers = numPassengers || 1;
+  //   price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 5);
+createBooking('LH123', undefined, 1000);
+
+const flight = 'LH234';
+const dan = {
+  name: 'Daniel Hill',
+  passport: 23456789,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 23456789) {
+    alert('Checked In');
+  } else {
+    alert('Wrong Passport!');
+  }
+};
+
+// checkIn(flight, dan);
+// console.log(flight);
+// console.log(dan);
+
+// const flightNum = flight;
+// const passenger = dan;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000);
+};
+
+newPassport(dan);
+checkIn(flight, dan);
+*/
