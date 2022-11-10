79ca67b6f2188ad3b56440c2d3649034d3b90a6f
danhillida <dan.hill.ida@gmail.com>
2022-11-09 19:42:00 -0500

    Worked on array methods

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -71,6 +71,82 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+/*
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// SLICE -- copies part of the array, doesn't mutate it
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+
+// Shallow Copy
+console.log(arr.slice());
+console.log([...arr]);
+
+// SPLICE -- deletes the part of array that is returned
+// console.log(arr.splice(2));
+arr.splice(-1);
+console.log(arr);
+arr.splice(1, 2);
+console.log(arr);
+
+// REVERSE -- does change the array
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+// CONCAT -- doesn't mutate original array
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log(...arr, ...arr2);
+
+console.log(letters.join(' - '));
+
+
+// AT METHOD --
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+// If we didn't know the last number
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+console.log(arr.at(-2));
+// At method best for last number or method chaining, quick value you should use bracket notation
+console.log('Daniel'.at(0)); //at works on strings too
+
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1} You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1} You withdrew ${Math.abs(movement)}`); //abs is absolute value
+  }
+}
+
+console.log('------------ For Each -------------');
+
+movements.forEach(function (movement, i, arr) {
+  // forEach gives us the (value, index, array) in that order
+  if (movement > 0) {
+    console.log(`Movement ${i + 1} You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1} You withdrew ${Math.abs(movement)}`); //abs is absolute value
+  }
+});
+// Continue and break statments do not work with forEach loop
+// How for each works
+// 0: function(200)
+// 1: function(450)
+// 2: function(400)
+*/
