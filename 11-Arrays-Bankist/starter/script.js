341138946de566bb2460a2b1a48ed49e84fe86fe
danhillida <dan.hill.ida@gmail.com>
2022-11-16 19:57:21 -0500

    worked on array methods coding challenge

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -602,7 +602,7 @@ console.log(z);
 // );
 // console.log(diceRolls);
 
-*/
+
 
 // Array Methods Practice
 
@@ -624,7 +624,7 @@ const numDeposits1000 = accounts
   .flatMap(acc => acc.movements)
   .reduce(
     (sum, curr) =>
-      curr >= 1000 ? ++sum /*must use the prefex increment*/ : sum,
+      curr >= 1000 ? ++sum : sum,
     0
   );
 console.log(numDeposits1000);
@@ -671,3 +671,46 @@ const convertTitleCase = function (title) {
 console.log(convertTitleCase('this is a nice title'));
 console.log(convertTitleCase('this is a LONG title'));
 console.log(convertTitleCase('and here is another title with an EXAMPLE'));
+*/
+
+//TEST DATA:
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+// 1
+const recommendedFoodFunc = function (dog) {
+  dog.forEach(weight => {
+    const recFood = Math.trunc(weight.weight ** 0.75 * 28);
+    weight.recommendedFood = recFood;
+  });
+};
+
+recommendedFoodFunc(dogs);
+console.log(dogs);
+
+// 2
+const dogSarah = dogs.find(dog => dog.owners.includes('Sarah'));
+console.log(
+  dogSarah.curFood > dogSarah.recommendedFood
+    ? "Sarah's dog is eating too much"
+    : "Sarah's dog is eating enough"
+);
+
+const ownersEatTooMuch = dogs
+  .filter(dog => dog.curFood > dog.recommendedFood)
+  .flatMap(dog => dog.owners);
+console.log(ownersEatTooMuch);
+
+const ownersEatTooLittle = dogs
+  .filter(dog => dog.curFood < dog.recommendedFood)
+  .flatMap(dog => dog.owners);
+console.log(ownersEatTooLittle);
+
+console.log(`${ownersEatTooMuch.join(' and ')}'s dogs eat too much!'`);
+console.log(`${ownersEatTooLittle.join(' and ')}'s dogs eat too little!'`);
+
+console.log(dogs.some(dog => dog.curFood === dog.recommendedFood));
